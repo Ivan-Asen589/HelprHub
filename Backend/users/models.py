009f503f7b176585b6b0294a090da947938c288f
Bob Tsan <Boris T>
2026-03-26 22:08:28 +0200

    Change

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -2,13 +2,10 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 
-
-# Create your models here.
 class User(AbstractUser):
-    #username = models.CharField(max_length=30)
-    #password = models.CharField(max_length=30)
     phone_number = PhoneNumberField()
     town = models.CharField(max_length=30)
     neighborhood = models.CharField(max_length=30)
-
-
+    
+    # ADD THIS LINE BELOW:
+    user_role = models.CharField(max_length=20, null=True, blank=True)
