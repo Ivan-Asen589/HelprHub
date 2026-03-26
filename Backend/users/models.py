e1f9bd46e525026efecbe327847cfbb62bac17c8
IvanEzz6 <ivan.o.ezekiev@gmail.com>
2026-03-26 12:14:58 +0200

    fixed type error (maybe?)

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_lenght=30)
-    password = models.CharField(max_lenght=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
     town = models.CharField(max_length=30)
     neighborhood = models.CharField(max_length=30)
