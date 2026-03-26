952e9453f9dd8b2400b1a8780cc52b70ba4f8023
Bob Tsan <Boris T>
2026-03-26 16:53:31 +0200

    Resolved merge conflict

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -21,23 +21,11 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-<<<<<<< HEAD
-    path('login/', views.login, name='login'), # Added /
-    
-    path('signup/', views.signup, name='signup'), 
-
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'), # Your change
     path('nujdaeshti/', views.nujdaeshti, name='nujdaeshti'),
     path('pomagashti/', views.pomagashti, name='pomagashti'),
     path('posts/', include('posts.urls')),
     path('helper_selector/', views.nujdaeshti, name='helper_selector'),
+    path('profil_potrebitel/', views.profil_potrebitel, name='profil_potrebitel'), # Teammate's change
 ]
-=======
-    path('login', views.login, name='login'),
-    path('nujdaeshti', views.nujdaeshti, name='nujdaeshti'),
-    path('pomagashti', views.pomagashti, name='pomagashti'),
-    path('posts/', include('posts.urls')),
-    path('helper_selector', views.nujdaeshti, name='helper_selector'),
-    path('profil_potrebitel', views.profil_potrebitel, name='profil_potrebitel'),
-    
-    ]
->>>>>>> 8d8ef3ce3023db157ad63bcaaface84fb3583d85
