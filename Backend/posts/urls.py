83f9f76ec7ce0970990fd7bd720ef617205e53fb
Ivan-Asen <ivanasen77@gmail.com>
2026-03-26 14:14:46 +0200

    Posts (additional functions)

--- Backend/posts/urls.py
+++ Backend/posts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_post, name='create_post'),
+    path('', views.post_list, name='post-list'),
+    path('create/', views.create_post, name='create_post'),
+    path('<int:pk>/edit/', views.edit_post, name='edit_post'),
+    path('<int:pk>/delete/', views.delete_post, name='delete_post'),
 ]
