fd33b68d955f58a11c78c6b3df68766938609bbb
Ivan-Asen <ivanasen77@gmail.com>
2026-03-26 14:42:11 +0200

    Sign Up Function Fix

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,45 +1,38 @@
-from django.shortcuts import render
-from models.py import User
+from django.shortcuts import redirect, render
+from .models import User
 
-# Create your views here.
-"""
-conditions:
-SignUp:
 
-"""
 def VerifySignUp(username: str, password: str, email=None, phone_number=None):
     minLen = 8
     maxLen = 30
-    cond = [False for i in range(6)]
-    
-    cond[0] = not any(elem.username==username for elem in User.objects.all()) #not: username already exists?
-    
-    if len(username)<=maxLen:
-        cond[1]=True #not: username too long?
-    
-    if len(password)>=minLen:
-        cond[2]=True
-    
-    if len(password)<=maxLen:
-        cond[3]=True
-    
+    cond = [False for i in range(7)]
+    cond[0] = not any(elem.username == username for elem in User.objects.all())  # username not already taken?
+
+    if len(username) <= maxLen:
+        cond[1] = True  # username not too long?
+
+    if len(password) >= minLen:
+        cond[2] = True
+
+    if len(password) <= maxLen:
+        cond[3] = True
+
     cond[4] = any(char.isdigit() for char in password)
-
     cond[5] = any(char.isupper() for char in password)
-
     cond[6] = any(char.islower() for char in password)
-
     return cond
 
-def SignUp(username_: str, password_: str, email_=None, phone_number_=None):
-    User.objects.create(
-        username=username_,
-        password=password_,
-        email=email_,
-        phone_number=phone_number_
-    )
 
-        
-    
-        
-    
+def SignUp(request):
+    if request.method == 'POST':
+        User.objects.create(
+            username=request.POST.get('username'),
+            password=request.POST.get('password'),
+            email=request.POST.get('email'),
+            phone_number=request.POST.get('phone_number'),
+            town=request.POST.get('town'),
+            neighborhood=request.POST.get('neighborhood'),
+        )
+        return redirect('login')
+
+    return render(request, 'home.html')
