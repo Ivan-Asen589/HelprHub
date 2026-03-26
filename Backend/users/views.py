24c9b28c418da1660daf2bb327bc1c0b0acf683f
Ivan-Asen <ivanasen77@gmail.com>
2026-03-26 21:14:28 +0200

    Added Sign Up back in

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -37,4 +37,51 @@ class UserDeleteView(DeleteView):
     success_url = '/login/'
 
     def get_object(self, queryset=None):
-        return self.request.
+        return self.request
+    
+def VerifySignUp(request):
+    if request.method != 'POST':
+        return None
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    minLen = 8
+    maxLen = 30
+    cond = [False for i in range(7)]
+    cond[0] = not User.objects.filter(username=username).exists() 
+    cond[1] = len(username) <= maxLen
+    cond[2] = len(password) >= minLen
+    cond[3] = len(password) <= maxLen
+    cond[4] = any(char.isdigit() for char in password)
+    cond[5] = any(char.isupper() for char in password)
+    cond[6] = any(char.islower() for char in password)
+    return cond
+
+def SignUp(request):
+    if request.method != 'POST':
+        return render(request, 'signup.html') 
+    ver = VerifySignUp(request)
+    if False not in ver: 
+        user = User.objects.create_user(  
+            username=request.POST.get('username'),  
+            email=request.POST.get('email'),
+            password=request.POST.get('password'),
+        )
+        login(request, user)  
+        return redirect('/')  
+    else:
+        errorsList = []
+        if not ver[0]:
+            errorsList.append('Username already exists!')
+        elif not ver[1]:
+            errorsList.append('Username too long (>30)!')
+        if not ver[2]:
+            errorsList.append('Password too short (<8)!')
+        elif not ver[3]:
+            errorsList.append('Password too long (>30)!')
+        if not ver[4]:
+            errorsList.append('Password has no digit!')
+        if not ver[5]:
+            errorsList.append('Password has no uppercase letter!')
+        if not ver[6]:
+            errorsList.append('Password has no lowercase letter!')
+        return render(request, 'signup.html', {'errors': errorsList})
