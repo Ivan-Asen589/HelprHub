7c205f06d1612fae7f5397bf30e7bba8592504a6
Ivan-Asen <ivanasen77@gmail.com>
2026-03-27 09:25:21 +0200

    Added Permisions

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -23,6 +23,10 @@ def VerifySignUp(request):
 
 
 def SignUp(request):
+    if request.user.is_authenticated:
+        if request.user.user_role == 'helper':
+            return redirect('helper_selector')
+        return redirect('nujdaeshti')
     if request.method != 'POST':
         return render(request, 'signup.html')
     ver = VerifySignUp(request)
@@ -61,6 +65,10 @@ def SignUp(request):
 
 
 def LogIn(request):
+    if request.user.is_authenticated:
+        if request.user.user_role == 'helper':
+            return redirect('helper_selector')
+        return redirect('nujdaeshti')
     if request.method == 'POST':
         user = authenticate(request, email=request.POST.get('email'), password=request.POST.get('password'))
         if user is not None:
