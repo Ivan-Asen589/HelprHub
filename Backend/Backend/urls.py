a3c8b59b124d5da677de23bf7b68a43350e54784
Bob Tsan <Boris T>
2026-03-27 09:49:13 +0200

    Changes

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
-from users.views import SignUp, LogIn, profile
+from users.views import SignUp, LogIn, LogOut, profile # Added LogOut here
 from posts.views import PostCreateView, PostListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', LogIn, name='login'),
+    path('logout/', LogOut, name='logout'), # Added this line
     path('signup/', SignUp, name='signup'),
     path('nujdaeshti/', PostCreateView.as_view(), name='nujdaeshti'),
     path('pomagashti/', views.pomagashti, name='pomagashti'),
@@ -15,3 +18,7 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('profile/', profile, name='profile'),
 ]
+
+# This is CRITICAL for showing profile pictures during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
