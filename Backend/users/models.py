d0a49f809daae2a96cd12723fa9e3551687e942e
Ivan-Asen <ivanasen77@gmail.com>
2026-03-28 03:57:15 +0200

    Fixed Posts in User Model

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -15,24 +15,3 @@ class User(AbstractUser):
         ('receiver', 'Receiver'),
     ]
     user_role = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='helper')
-
-class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    description = models.TextField(max_length=1000)
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    locationTown = models.CharField(max_length=100)
-    locationNeighborhood = models.CharField(max_length=200)
-    
-    volunteer = models.ForeignKey(
-        User, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        blank=True, 
-        related_name='users_volunteered_posts'
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.description[:20]}"
