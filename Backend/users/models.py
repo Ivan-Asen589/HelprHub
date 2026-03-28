0f1c77ab5702f82fd2740ba75c079f5938f40de9
Ivan-Asen <ivanasen77@gmail.com>
2026-03-28 02:12:14 +0200

    Increased Max Len of Neighborhood to 70 chars

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     username = models.CharField(max_length=30, unique=True)
     phone_number = PhoneNumberField(blank=True)
     town = models.CharField(max_length=30, blank=True)
-    neighborhood = models.CharField(max_length=30, blank=True)
+    neighborhood = models.CharField(max_length=70, blank=True)
     description = models.TextField(max_length=500, null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     
